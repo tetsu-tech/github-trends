eeafa6498c5c9d511363354b34042486d06bdd08
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-03 00:00:37 +0900

    delete env

--- sendMessage.js
+++ sendMessage.js
@@ -1,46 +1,22 @@
 require('dotenv').config();
+const app = require('./app');
 
-const { App } = require('@slack/bolt');
+const id = process.env.CHANNEL_ID;
 
-const app = new App({
-	token: process.env.SLACK_BOT_TOKEN,
-	signingSecret: process.env.SLACK_SIGNING_SECRET
-})
-
-const sendMessage = async (id, text) => {
+const sendMessage = async (id, url) => {
 	try {
-		// Call the chat.postMessage method using the built-in WebClient
+		// Todo: この関数だと、メッセージが1つ1つ分かれてしまうから、まとめて送れるようにしたい
 		const result = await app.client.chat.postMessage({
-			// The token you used to initialize your app
 			token: process.env.SLACK_BOT_TOKEN,
 			channel: id,
-			text: text,
-			blocks: [
-				{
-					"type": "section",
-					"text": {
-						"type": "plain_text",
-						"text": text,
-					}
-				}
-			]
-			// You could also use a blocks[] array to send richer content
+			text: url,
+			attachments: url,
 		});
 
-		// Print result, which includes information about the message (like TS)
 		console.log(result);
 	} catch (error) {
     console.error(error);
 	}
 }
 
-// sendMessageをexportする
-
-// これ以降の処理はindexの中で呼ぶようにする
-sendMessage("C01SSQ9EAN8", "Hello world :tada:");
-
-(async () => {
-  await app.start(process.env.PORT || 3500);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+module.exports = sendMessage;
