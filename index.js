8c60ea341181222f0ca0222362c733c6a9d1b210
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-02 15:44:18 +0900

    get trends repo url

--- index.js
+++ index.js
@@ -8,25 +8,28 @@ const port = process.env.PORT || 3400
 
 app.get('/api/v1/', (req, res) => {
 
-	const get_trends = async () => {
+	const getTrends = async () => {
 		try {
 			const response = await githubTrends({ section: 'developers', since: 'weekly' })
-
-			// console.log(typeof trends)
 
 			const trends = (Object.entries(response).map((trend) => {
 				return trend[1]
 			}));
 
+			const repoUrls = trends.map((repoUrl) => {
+				return repoUrl.repourl
+			})
+
 			console.log(trends)
 
-			res.json(trends);
+			// res.json(trends[0]);
+			res.json(repoUrls);
 		} catch (error) {
 			res.send(error.message)
 		}
 	}
 
-	get_trends();
+	getTrends();
 
 })
 
