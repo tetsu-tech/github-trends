e4581f0ed0488b1329fd0cf2a4e9147d75e99302
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-02 14:29:28 +0900

    render trends json

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const githubTrends = require('github-trends-api')
 const port = process.env.PORT || 3400
 
 app.get('/api/v1/', (req, res) => {
-	const trends = githubTrends({ section: 'developers', since: 'weekly' }).then(
-		result => { return result }
-	).catch(
-		error => { console.log(error) }
-	)
-
-	console.log(trends);
+	try {
+		const trends = githubTrends({ section: 'developers', since: 'weekly' }).then(
+			result => { 
+				console.log(result)
+				res.json(result)
+			}
+		).catch(
+			error => { console.log(error) }
+		)
+	} catch (err) {
+		console.log(err.message);
+	}
 
 })
 
