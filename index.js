cbc6295a3540461f2a963a817e291bf56fa66fca
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-10-02 22:46:05 +0900

    fix send message func

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const http = require('http');
 
 const githubTrends = require('github-trends-api')
 
 const port = process.env.PORT || 3400
 
-app.get('/api/v1/', (req, res) => {
 
-	const getTrends = async () => {
-		try {
-			const response = await githubTrends({ section: 'developers', since: 'weekly' })
+const getTrends = async () => {
+	try {
+		const response = await githubTrends({ section: 'developers', since: 'weekly' })
 
-			const trends = (Object.entries(response).map((trend) => {
-				return trend[1]
-			}));
+		const trends = (Object.entries(response).map((trend) => {
+			return trend[1]
+		}));
 
-			const repoUrls = trends.map((repoUrl) => {
-				return repoUrl.repourl
-			})
+		const repoUrls = trends.map((repoUrl) => {
+			return repoUrl.repourl
+		})
 
-			console.log(trends)
+		// sendMessageを呼び出して。repoUrlを引数に入れる
+		// sendMessage();
 
-			// res.json(trends[0]);
-			res.json(repoUrls);
-		} catch (error) {
-			res.send(error.message)
-		}
+		return repoUrls;
+	} catch (error) {
+		console.log(error)
+		process.exit(1);
 	}
+}
 
-	getTrends();
-
-})
-
-app.listen(port);
-console.log('listen on port ' + port);
+// const server = http.createServer(function(req, res) {
+// 	getTrends();
+// 	res.end('success')
+// }).listen(port);
--- sendMessage.js
+++ sendMessage.js
@@ -7,14 +7,14 @@ const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET
 })
 
-async function publishMessage(id, text) {
-  try {
-    // Call the chat.postMessage method using the built-in WebClient
-    const result = await app.client.chat.postMessage({
-      // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: id,
-      text: text,
+const sendMessage = async (id, text) => {
+	try {
+		// Call the chat.postMessage method using the built-in WebClient
+		const result = await app.client.chat.postMessage({
+			// The token you used to initialize your app
+			token: process.env.SLACK_BOT_TOKEN,
+			channel: id,
+			text: text,
 			blocks: [
 				{
 					"type": "section",
@@ -24,18 +24,20 @@ async function publishMessage(id, text) {
 					}
 				}
 			]
-      // You could also use a blocks[] array to send richer content
-    });
+			// You could also use a blocks[] array to send richer content
+		});
 
-    // Print result, which includes information about the message (like TS)
-    console.log(result);
-  }
-  catch (error) {
+		// Print result, which includes information about the message (like TS)
+		console.log(result);
+	} catch (error) {
     console.error(error);
-  }
+	}
 }
 
-publishMessage("C01SSQ9EAN8", "Hello world :tada:");
+// sendMessageをexportする
+
+// これ以降の処理はindexの中で呼ぶようにする
+sendMessage("C01SSQ9EAN8", "Hello world :tada:");
 
 (async () => {
   await app.start(process.env.PORT || 3500);
